aa65b6b38ab77e92bf4edebbd1fb6a5ac2c0ca4f
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-18 14:57:22 +0800

    feat: add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ScrollAlphaTestnet } from "@thirdweb-dev/chains";
 import NavBar from "../components/Navbar";
+import Head from "next/head";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={ScrollAlphaTestnet}>
       <ChakraProvider>
+        <Head><title>Crypto Farmer - Idle on-chain game</title></Head>
         <NavBar />
         <Component {...pageProps} />
       </ChakraProvider>

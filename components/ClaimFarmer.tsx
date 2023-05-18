1667c89ca6b4ef59f28969bb6975fbbf4ed50d7e
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-18 13:50:09 +0800

    feat: add claim farmer

---
+++ components/ClaimFarmer.tsx
@@ -0,0 +1,32 @@
+import { MediaRenderer, Web3Button, useContract, useContractMetadata } from "@thirdweb-dev/react";
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+
+import { FARMER_ADDRESS } from "../const/addresses";
+
+export function ClaimFarmer() {
+    const { contract } = useContract(FARMER_ADDRESS);
+    const { data: metadata } = useContractMetadata(contract);
+
+    console.log(metadata)
+
+    return (
+        <Container maxW={"1200px"}>
+            <Flex direction={"column"} alignItems={"center"} justifyContent={"center"} h={"50vh"}>
+                <Heading>Claim Farmer to start farming</Heading>
+                <Box borderRadius={"8px"} overflow={"hidden"} my={10}>
+                    <MediaRenderer
+                        src='/images/farmer.png'
+                        // src={metadata?.image}
+                        height="300px"
+                        width="300px"
+                    />
+                </Box>
+
+                <Web3Button
+                    contractAddress={FARMER_ADDRESS}
+                    action={(contract) => contract.erc1155.claim(0, 1)}
+                >Claim Farmer</Web3Button>
+            </Flex>
+        </Container>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from "next";
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
-import { Container, Flex, Heading } from "@chakra-ui/react";
+import { ConnectWallet, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import { Container, Flex, Heading, Spinner } from "@chakra-ui/react";
+import { FARMER_ADDRESS, TOOLS_ADDRESS, REWARDS_ADDRESS, STAKING_ADDRESS } from "../const/addresses";
+import { ClaimFarmer } from "../components/ClaimFarmer";
 
 const Home: NextPage = () => {
   const address = useAddress();
+
+  // setup all contracts
+  const { contract: farmerContract } = useContract(FARMER_ADDRESS);
+  const { contract: toolsContract } = useContract(TOOLS_ADDRESS);
+  const { contract: rewardsContract } = useContract(REWARDS_ADDRESS);
+  const { contract: stakingContract } = useContract(STAKING_ADDRESS);
+
+  // check owned nfts from the farmer contract
+  const { data: ownedFarmers, isLoading: loadingOwnedFarmers } = useOwnedNFTs(farmerContract, address)
 
   // if not logged in, show login button
   if (!address) {
@@ -13,6 +24,26 @@ const Home: NextPage = () => {
           <Heading my={"40px"}>Welcome to Crypto Farmer</Heading>
           <ConnectWallet />
         </Flex>
+      </Container>
+    )
+  }
+
+  // if loading, show loading
+  if (loadingOwnedFarmers) {
+    return (
+      <Container maxW={"1200px"}>
+        <Flex h={"100vh"} justifyContent={"center"} alignItems={"center"}>
+          <Spinner />
+        </Flex>
+      </Container>
+    )
+  }
+
+  // if user has no farmer, show claim farmer button
+  if (ownedFarmers?.length === 0) {
+    return (
+      <Container maxW={"1200px"}>
+        <ClaimFarmer />
       </Container>
     )
   }
